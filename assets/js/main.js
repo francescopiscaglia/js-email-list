7fbd780d8230f119f750557c15a9f4349e096172
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-17 16:19:29 +0200

    add event listener

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,28 +2,38 @@
 const listEl = document.querySelector(".container > ol");
 // console.log(listEl);
 
-for (let i = 0; i < 10; i++) {
+const buttonEl = document.querySelector(".navbar .btn");
+// console.log(buttonEl);
 
-    // faccio la chiamata all'API di boolean
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+// event listener
+buttonEl.addEventListener("click", () => {
+    listEl.innerHTML = "";
+
+    for (let i = 0; i < 10; i++) {
     
-    // quando il risultato è pronto lo richiamo nella funzione response
-    .then(response => {
+        // faccio la chiamata all'API di boolean
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        
+        // quando il risultato è pronto lo richiamo nella funzione response
+        .then(response => {
+    
+            // salvo response.data, ovvero la email generata in una costante e il valore booleano true 
+            const result = response.data
+            // console.log(result);
+    
+            // salvo la mail 
+            const resultEmail = result.response;
+            // console.log(resultEmail);
+    
+            const listItemEl = document.createElement("li");
+            listItemEl.classList.add("list-group-item")
+            listItemEl.innerText = resultEmail
+    
+            listEl.appendChild(listItemEl);
+        })
+        .catch(error => console.error(error))
+    };
+});
 
-        // salvo response.data, ovvero la email generata in una costante e il valore booleano true 
-        const result = response.data
-        // console.log(result);
-
-        // salvo la mail 
-        const resultEmail = result.response;
-        // console.log(resultEmail);
-
-        const listItemEl = document.createElement("li");
-        listItemEl.classList.add("list-group-item")
-        listItemEl.innerText = resultEmail
-
-        listEl.appendChild(listItemEl);
-    })
-    .catch(error => console.error(error))
-};
 
